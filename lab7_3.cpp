ab6ed85ed35ec66236f19d4af5a7f7ad8633ee51
BoegyNat <bboegy@gmail.com>
2021-11-30 18:32:31 +0700

    Lap complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 
+int adiff(int a,int b){
+  int c = a - b ;
+  if (c < 0){
+    c = c * -1;
+  }
+  c = c%360;
+  if (c > 180){
+    c = 360 - c;
+  }
+  return c;
+
+}
 
 int main(){
   cout << adiff(180,270);
